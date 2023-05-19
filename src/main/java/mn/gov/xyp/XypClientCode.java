aa9466720d5c23dde5443c832319b9bd2951451f
gerle <gereltsetseg@ema.gov.mn>
2023-05-19 11:08:57 +0800

    create opt function

--- src/main/java/mn/gov/xyp/XypClientCode.java
+++ src/main/java/mn/gov/xyp/XypClientCode.java
@@ -13,10 +13,8 @@ public class XypClientCode {
         Map<String, Object> req_ctx = ((BindingProvider)citizenService).getRequestContext();
         req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, wsdl);
         Map<String, List<String>> headers = new HashMap<String, List<String>>();
-        System.out.println("here callUseSignature");
         XypSign xypSign = new XypSign();
         Hashtable<String, String> fields = xypSign.Generate(Constants.ACCESS_TOKEN, timestamp);
-        System.out.println("here callUseSignature 2 ");
         try{
             headers.put("accessToken", Collections.singletonList(fields.get("accessToken")));
             headers.put("timestamp", Collections.singletonList(fields.get("timestamp")));
@@ -27,7 +25,6 @@ public class XypClientCode {
             AuthorizationData authorizationData = new AuthorizationData();
             AuthorizationEntity authorizationEntity = new AuthorizationEntity();
 
-            System.out.println("Fsdfsdfdsf");
             authorizationEntity.setRegnum(regnum);
             authorizationEntity.setCertFingerprint(serialNumber);
             authorizationEntity.setSignature(signature);
@@ -48,5 +45,59 @@ public class XypClientCode {
             System.out.println(e.getMessage());
             System.exit(1);
         }
+    }
+
+    public void callUseOTP(String timestamp, String regnum) {
+        int otpCode = 0;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Иргэнд ирсэн OTP код: ");
+        otpCode = sc.nextInt();
+        sc.close();
+
+        if(otpCode == 0){
+            System.out.println("OTP код буруу байна.");
+            return;
+        }
+
+        CitizenService citizenService = new CitizenServiceService().getCitizenServicePort();
+        Map<String, Object> req_ctx = ((BindingProvider)citizenService).getRequestContext();
+        req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, wsdl);
+        Map<String, List<String>> headers = new HashMap<String, List<String>>();
+        XypSign xypSign = new XypSign();
+        Hashtable<String, String> fields = xypSign.Generate(Constants.ACCESS_TOKEN, timestamp);
+        try{
+            headers.put("accessToken", Collections.singletonList(fields.get("accessToken")));
+            headers.put("timestamp", Collections.singletonList(fields.get("timestamp")));
+            headers.put("signature", Collections.singletonList(fields.get("signature")));
+            req_ctx.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
+
+            CitizenRequestData requestData = new CitizenRequestData();
+            AuthorizationData authorizationData = new AuthorizationData();
+            AuthorizationEntity authorizationEntity = new AuthorizationEntity();
+
+            authorizationEntity.setRegnum(regnum);
+            authorizationEntity.setOtp(otpCode);
+
+            authorizationData.setCitizen(authorizationEntity);
+
+            requestData.setRegnum(regnum);
+            requestData.setAuth(authorizationData);
+            ServiceResponse serviceResponse = citizenService.ws100101GetCitizenIDCardInfo(requestData);
+
+            System.out.println(serviceResponse.getResultCode());
+            System.out.println(serviceResponse.getResultMessage());
+            System.out.println(serviceResponse.getRequestId());
+            System.out.println(serviceResponse.getResponse());
+
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        XypClientCode clientCode = new XypClientCode();
+        clientCode.callUseOTP(DigitalSignatureApprove.GetCurrentTimestamp(), "ЕЮ00222501");
     }
 }
