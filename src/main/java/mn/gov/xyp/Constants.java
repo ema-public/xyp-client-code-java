ed6c28cad058d255624a763132b863d7500a4886
gerle <gereltsetseg@ema.gov.mn>
2023-05-19 12:02:04 +0800

    remove sensitive data

--- src/main/java/mn/gov/xyp/Constants.java
+++ src/main/java/mn/gov/xyp/Constants.java
@@ -3,6 +3,6 @@ package mn.gov.xyp;
 public class Constants {
     public static final String ESIGN_URL = "ws://127.0.0.1:59001";
     public static final String KEY_PATH = "src/key.key";
-    public static final String ACCESS_TOKEN = "9f03863d2f8ae93b24a47c6d3e0314d1";
-    public static final String REGNUM = "ЕЮ00222501";
+    public static final String ACCESS_TOKEN = "qwertyuoisdfjlkm12dfgklskd4";
+    public static final String REGNUM = "АА00112233";
 }
